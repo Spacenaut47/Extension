e913c9bcbbcee8a94bbb7d78682db70ce1e0b74a
Spacenaut47 <sohammondal81@gmail.com>
2022-04-05 19:03:57 +0530

    Added Functionality to Buttons

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-let a = document.querySelector("#input-btn")
+let myLeads = {}
+const inputEl = document.querySelector("#input-el")
+const a = document.querySelector("#input-btn")
+inputEl.textContent = "lol"
 a.addEventListener("click", function(){
-    console.log("button");
+    console.log("button"); 
 })
