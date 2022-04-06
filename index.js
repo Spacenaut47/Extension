210dc41f7c4b50bab74cadde8d39022bfbc3424d
Spacenaut47 <sohammondal81@gmail.com>
2022-04-06 08:27:44 +0530

    Render Leads

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-let myLeads = ["weads","weadsads","dassaed"]
+let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const a = document.querySelector("#input-btn")
 const b = document.querySelector("#unord")
 a.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    renderLeads()
 })
-let listItems = ""
-for(let i = 0; i < myLeads.length;i++){
-    listItems += "<li>"+myLeads[i]+"</li> "
+function renderLeads ()
+{
+    let listItems = ""
+    for(let i = 0; i < myLeads.length;i++){
+
+        listItems += "<li>"+myLeads[i]+"</li> "
+    }
+    b.innerHTML = listItems
 }
-b.innerHTML = listItems
