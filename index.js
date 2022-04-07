d0ce7eb54c368fc0caee8e0d8e07aba56ca00e05
Spacenaut47 <sohammondal81@gmail.com>
2022-04-07 20:31:21 +0530

    Added Delete Button

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const a = document.querySelector("#input-btn")
 const b = document.querySelector("#unord")
+const c = document.querySelector("#delete-all")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 a.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = " "
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
-    console.log(localStorage.getItem("myLeads"));
+})
+c.addEventListener("click", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
 })
 function renderLeads ()
 {
@@ -22,5 +33,3 @@ function renderLeads ()
     }
     b.innerHTML = listItems
 }
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)
